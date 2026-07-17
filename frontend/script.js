42c767e502718206dc03615ca6c230fe823d9202
Hemalatha <hemalathaab1205@gmail.com>
2026-07-17 11:38:16 +0530

    Connect visitor counter

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
-document.getElementById("count").innerHTML = "0";
+const apiUrl = "https://fc6a2xdkkjizk5o4uh5ygniyiu0wqhwd.lambda-url.eu-north-1.on.aws/";
+
+async function updateCounter() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        document.getElementById("visitor-count").innerText =
+            data.count;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+updateCounter();
